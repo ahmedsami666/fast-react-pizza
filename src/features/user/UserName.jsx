cdc4be7c695683c80f3a8c6bc5adbc990691f898
AhmedSami <71245088+ahmedsami666@users.noreply.github.com>
2024-08-30 15:28:30 +0300

    setting up redux toolkit

--- src/features/user/UserName.jsx
+++ src/features/user/UserName.jsx
@@ -1,6 +1,10 @@
+import { useSelector } from "react-redux"
+
 const UserName = () => {
+    const { userName } = useSelector(state => state.user)
+    if (!userName) return null
     return (
-        <div className="text-sm font-semibold hidden md:block">Jone</div>
+        <div className="text-sm font-semibold hidden md:block">{userName}</div>
     )
 }
 export default UserName
